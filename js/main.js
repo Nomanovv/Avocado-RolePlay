4f245375a4895d51e8444db59ea29abf4c6911a7
Nomanovv <88490331+Nomanovv@users.noreply.github.com>
2021-08-05 14:39:51 +0300

    Developed Avocado Site

---
+++ js/main.js
@@ -0,0 +1,1 @@
+"use strict";$(".mobile div").on("click",(function(){$(".mobile div").toggleClass("active"),$(".mobile nav").toggleClass("open"),$(".mobile nav ul").toggleClass("show")}));for(var a=1;a<=$(".mobile ul li").length;a++)$(".mobile ul li:nth-child("+a+")").css("animation-delay","."+(a+1)+"s");
